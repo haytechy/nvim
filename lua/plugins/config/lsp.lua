92e028c6c0c7b10082c024db64882f32313a02fb
haytehcy <alexhayman89@gmail.com>
2024-12-20 09:12:31 +0000

    Changed Config

--- lua/plugins/config/lsp.lua
+++ lua/plugins/config/lsp.lua
@@ -1,4 +1,8 @@
 local capabilities = require('blink.cmp').get_lsp_capabilities()
+local servers = { "pyright", "clangd"}
+for _, server in pairs(servers) do
+    require('lspconfig')[server].setup{capabilities = capabilities}
+end
 require('lspconfig').pyright.setup{}
 require('lspconfig').lua_ls.setup{
     capabilities = capabilities,
